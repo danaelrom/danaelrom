745fc47a672fb7f2bdaa3e67a26ebab7ff3585d4
Dana Elrom <154305333+danaelrom@users.noreply.github.com>
2023-12-21 20:56:14 +0200

    Delete homework1 directory

--- homework1/Coins.java
+++
@@ -1,8 +0,0 @@
-public class Coins {
-    public static void main(String[] args) {
-        int number = Integer.parseInt(args[0]);
-        int quarters = number / 25 ;
-        int cents = number % 25; 
-        System.out.println(" Use " + quarters +  " quarters " + " and " + cents +  " cents ");
-    }
-}
